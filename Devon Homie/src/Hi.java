fdda192541edf8b2842feb3208085cc0a37e43f9
nicklocascio <nicklocascio@192.168.0.13>
2016-10-24 20:46:09 -0600

    Whatup

---
+++ Devon Homie/src/Hi.java
@@ -0,0 +1,83 @@
+
+public static void horse()
+	{
+	
+	if(stable.get(0).getPoints() > stable.get(1).getPoints() ||
+		stable.get(0).getPoints() > stable.get(2).getPoints() ||
+		stable.get(0).getPoints() > stable.get(3).getPoints() ||
+		stable.get(0).getPoints() > stable.get(4).getPoints() ||
+		stable.get(0).getPoints() > stable.get(5).getPoints() ||
+		stable.get(0).getPoints() > stable.get(6).getPoints()
+		)
+		{
+		System.out.println(stable.get(0).getName());
+		}
+	
+	else if(stable.get(1).getPoints() > stable.get(0).getPoints() ||
+			stable.get(1).getPoints() > stable.get(2).getPoints() ||
+			stable.get(1).getPoints() > stable.get(3).getPoints() ||
+			stable.get(1).getPoints() > stable.get(4).getPoints() ||
+			stable.get(1).getPoints() > stable.get(5).getPoints() ||
+			stable.get(1).getPoints() > stable.get(6).getPoints() 
+			)
+			{
+			System.out.println(stable.get(1).getName());
+			}
+	
+	else if(stable.get(2).getPoints() > stable.get(0).getPoints() ||
+			stable.get(2).getPoints() > stable.get(1).getPoints() ||
+			stable.get(2).getPoints() > stable.get(3).getPoints() ||
+			stable.get(2).getPoints() > stable.get(4).getPoints() ||
+			stable.get(2).getPoints() > stable.get(5).getPoints() ||
+			stable.get(2).getPoints() > stable.get(6).getPoints()
+			)
+			{
+			System.out.println(stable.get(2).getName());
+			}
+	
+	else if(stable.get(3).getPoints() > stable.get(0).getPoints() ||
+			stable.get(3).getPoints() > stable.get(1).getPoints() ||
+			stable.get(3).getPoints() > stable.get(2).getPoints() ||
+			stable.get(3).getPoints() > stable.get(4).getPoints() ||
+			stable.get(3).getPoints() > stable.get(5).getPoints() ||
+			stable.get(3).getPoints() > stable.get(6).getPoints()
+			)
+			{
+			System.out.println(stable.get(3).getName());
+			}
+	
+	else if(stable.get(4).getPoints() > stable.get(0).getPoints() ||
+			stable.get(4).getPoints() > stable.get(1).getPoints() ||
+			stable.get(4).getPoints() > stable.get(2).getPoints() ||
+			stable.get(4).getPoints() > stable.get(3).getPoints() ||
+			stable.get(4).getPoints() > stable.get(5).getPoints() ||
+			stable.get(4).getPoints() > stable.get(6).getPoints()
+			)
+			{
+			System.out.println(stable.get(4).getName());
+			}
+	
+	else if(stable.get(5).getPoints() > stable.get(0).getPoints() ||
+			stable.get(5).getPoints() > stable.get(1).getPoints() ||
+			stable.get(5).getPoints() > stable.get(2).getPoints() ||
+			stable.get(5).getPoints() > stable.get(3).getPoints() ||
+			stable.get(5).getPoints() > stable.get(4).getPoints() ||
+			stable.get(5).getPoints() > stable.get(6).getPoints() 
+			)
+			{
+			System.out.println(stable.get(5).getName());
+			}
+	
+	else if(stable.get(6).getPoints() > stable.get(0).getPoints() ||
+			stable.get(6).getPoints() > stable.get(1).getPoints() ||
+			stable.get(6).getPoints() > stable.get(2).getPoints() ||
+			stable.get(6).getPoints() > stable.get(3).getPoints() ||
+			stable.get(6).getPoints() > stable.get(4).getPoints() ||
+			stable.get(6).getPoints() > stable.get(5).getPoints() 
+			)
+			{
+			System.out.println(stable.get(6).getName());
+			}
+		}
+
+//hi 
